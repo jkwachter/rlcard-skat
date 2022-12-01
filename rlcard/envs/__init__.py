4e934b2de3aa0ff5744c007ef4f374f12a9ca704
Jakob Wachter <jakob.wachter@gmail.com>
2022-12-01 12:18:33 -0500

    Registered skat as an environment

--- rlcard/envs/__init__.py
+++ rlcard/envs/__init__.py
@@ -5,22 +5,22 @@ from rlcard.envs.registration import register, make
 
 register(
     env_id='blackjack',
-    entry_point='rlcard.envs.blackjack:BlackjackEnv',
+    entry_point='rlcard.envs.blackjack:BlackjackEnv'
 )
 
 register(
     env_id='doudizhu',
-    entry_point='rlcard.envs.doudizhu:DoudizhuEnv',
+    entry_point='rlcard.envs.doudizhu:DoudizhuEnv'
 )
 
 register(
     env_id='limit-holdem',
-    entry_point='rlcard.envs.limitholdem:LimitholdemEnv',
+    entry_point='rlcard.envs.limitholdem:LimitholdemEnv'
 )
 
 register(
     env_id='no-limit-holdem',
-    entry_point='rlcard.envs.nolimitholdem:NolimitholdemEnv',
+    entry_point='rlcard.envs.nolimitholdem:NolimitholdemEnv'
 )
 
 register(
@@ -30,20 +30,25 @@ register(
 
 register(
     env_id='uno',
-    entry_point='rlcard.envs.uno:UnoEnv',
+    entry_point='rlcard.envs.uno:UnoEnv'
 )
 
 register(
     env_id='mahjong',
-    entry_point='rlcard.envs.mahjong:MahjongEnv',
+    entry_point='rlcard.envs.mahjong:MahjongEnv'
 )
 
 register(
     env_id='gin-rummy',
-    entry_point='rlcard.envs.gin_rummy:GinRummyEnv',
+    entry_point='rlcard.envs.gin_rummy:GinRummyEnv'
 )
 
 register(
     env_id='bridge',
-    entry_point='rlcard.envs.bridge:BridgeEnv',
+    entry_point='rlcard.envs.bridge:BridgeEnv'
+)
+
+register(
+    env_id='skat',
+    entry_point='rlcard.envs.skat:SkatEnv'
 )
